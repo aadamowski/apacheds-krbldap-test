84e0ce2f559fd3aa3c98a062ba77c882b267b79e
Aleksander Adamowski <aleksander.adamowski@gmail.com>
2011-10-22 15:32:45 +0200

    Employment of KerberosMessageContainer for KerberosMessage decoding.

--- src/main/java/pl/org/olo/krbldap/apacheds/extras/extended/ads_impl/krbLdap/KrbLdapDecoder.java
+++ src/main/java/pl/org/olo/krbldap/apacheds/extras/extended/ads_impl/krbLdap/KrbLdapDecoder.java
@@ -1,9 +1,26 @@
 package pl.org.olo.krbldap.apacheds.extras.extended.ads_impl.krbLdap;
 
+import java.nio.ByteBuffer;
+
+import org.apache.directory.shared.asn1.Asn1Object;
+import org.apache.directory.shared.asn1.DecoderException;
+import org.apache.directory.shared.asn1.ber.Asn1Container;
 import org.apache.directory.shared.asn1.ber.Asn1Decoder;
+import org.apache.directory.shared.kerberos.codec.KerberosMessageContainer;
 
 /**
  *
  */
 public class KrbLdapDecoder extends Asn1Decoder {
+    /**
+     * The decoder
+     */
+    private static final Asn1Decoder decoder = new Asn1Decoder();
+
+    public Asn1Object decode(byte[] stream) throws DecoderException {
+        ByteBuffer bb = ByteBuffer.wrap(stream);
+        final KerberosMessageContainer container = new KerberosMessageContainer();
+        decoder.decode(bb, container);
+        return container.getMessage();
+    }
 }
--- src/main/java/pl/org/olo/krbldap/apacheds/extras/extended/ads_impl/krbLdap/KrbLdapRequestDecorator.java
+++ src/main/java/pl/org/olo/krbldap/apacheds/extras/extended/ads_impl/krbLdap/KrbLdapRequestDecorator.java
@@ -1,7 +1,11 @@
 package pl.org.olo.krbldap.apacheds.extras.extended.ads_impl.krbLdap;
 
+import org.apache.directory.shared.asn1.DecoderException;
+import org.apache.directory.shared.kerberos.messages.KerberosMessage;
 import org.apache.directory.shared.ldap.codec.api.ExtendedRequestDecorator;
 import org.apache.directory.shared.ldap.codec.api.LdapApiService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import pl.org.olo.krbldap.apacheds.extras.extended.KrbLdapRequest;
 import pl.org.olo.krbldap.apacheds.extras.extended.KrbLdapResponse;
 
@@ -10,6 +14,8 @@ import pl.org.olo.krbldap.apacheds.extras.extended.KrbLdapResponse;
  */
 public class KrbLdapRequestDecorator extends ExtendedRequestDecorator<KrbLdapRequest, KrbLdapResponse>
         implements KrbLdapRequest {
+    private static final Logger LOG = LoggerFactory.getLogger(KrbLdapRequestDecorator.class);
+
     /**
      * Makes a ExtendedRequest a MessageDecorator.
      *
@@ -17,5 +23,16 @@ public class KrbLdapRequestDecorator extends ExtendedRequestDecorator<KrbLdapReq
      */
     public KrbLdapRequestDecorator(LdapApiService codec, KrbLdapRequest decoratedMessage) {
         super(codec, decoratedMessage);
+    }
+
+    @Override
+    public void setRequestValue(byte[] requestValue) {
+        final KrbLdapDecoder decoder = new KrbLdapDecoder();
+        try {
+            final KerberosMessage kerberosMessage = (KerberosMessage) decoder.decode(requestValue);
+            LOG.info("kerberosMessage: [" + kerberosMessage + "]");
+        } catch (DecoderException e) {
+            LOG.error("Error decoding KerbeLDAP message: " + e.getMessage(), e);
+        }
     }
 }
